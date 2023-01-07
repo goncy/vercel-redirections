71de46941196ba8d1077a1d99e9ae745e885afb7
goncy <gonzalo.pozzo4@gmail.com>
2023-01-07 09:15:58 -0300

    initial commit

---
+++ api/index.js
@@ -0,0 +1,12 @@
+import redirections from "../redirections.json"
+
+export const config = {
+  runtime: 'edge',
+};
+
+export default async (req) => {
+  const [subdomain] = req.headers.get('x-forwarded-host').split(`.`);
+  const redirection = redirections[subdomain] || 'https://goncy.twitter.com'; // Reemplazá con tu link por defecto
+
+  return Response.redirect(redirection, 302);
+};
